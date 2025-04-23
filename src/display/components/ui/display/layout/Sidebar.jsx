9bf01ced710afc734b697c94f54997655b39b19c
monority <chenu.pro@gmail.com>
2025-04-23 16:39:46 +0200

    :art: - enhance Sidebar component with additional sections and upgrade button

--- src/display/components/ui/display/layout/Sidebar.jsx
+++ src/display/components/ui/display/layout/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Input from './../../../utils/base/Input';
 import Icon from '../../../utils/Icon';
 import { useState } from 'react';
-
+import Button from '../../../utils/base/Button';
 const Sidebar = () => {
 	const [selected, setSelected] = useState(false);
 	return (
@@ -21,6 +21,9 @@ const Sidebar = () => {
 						<Input type="search" placeholder="Search" className="input_default" name="search" />
 					</div>
 					<div className="wrapper">
+						<div className="element">
+							<h4>Title</h4>
+						</div>
 						<ul className="sidebar__menu">
 							<li className={`${selected && 'selected'} text_size2`}>
 								<div className="element">
@@ -46,7 +49,7 @@ const Sidebar = () => {
 									<p>Task</p>
 								</div>
 							</li>
-					
+
 							<li className={`${selected && 'selected'} text_size2`}>
 								<div className="element">
 									<Icon type="home" size='1.5rem' />
@@ -56,6 +59,58 @@ const Sidebar = () => {
 								</div>
 							</li>
 						</ul>
+					</div>
+					<div className="wrapper">
+						<div className="element">
+							<h4>SubTitle</h4>
+						</div>
+						<ul className="sidebar__menu">
+							<li className={`${selected && 'selected'} text_size2`}>
+								<div className="element">
+									<Icon type="home" size='1.5rem' />
+								</div>
+								<div className="element">
+									<p>Dashboard</p>
+								</div>
+							</li>
+							<li className={`${selected && 'selected'} text_size2`}>
+								<div className="element">
+									<Icon type="home" size='1.5rem' />
+								</div>
+								<div className="element">
+									<p>Mail</p>
+								</div>
+							</li>
+							<li className={`${selected && 'selected'} text_size2`}>
+								<div className="element">
+									<Icon type="home" size='1.5rem' />
+								</div>
+								<div className="element">
+									<p>Task</p>
+								</div>
+							</li>
+							<li className={`${selected && 'selected'} text_size2`}>
+								<div className="element">
+									<Icon type="home" size='1.5rem' />
+								</div>
+								<div className="element">
+									<p>Orders</p>
+								</div>
+							</li>
+						</ul>
+					</div>
+					<div className="wrapper">
+						<div className="box__premium">
+							<div className="element">
+								<h4>Become Pro</h4>
+							</div>
+							<div className="element">
+								<p>Wanna go faster ?</p>
+							</div>
+							<div className="element">
+								<Button>Upgrade</Button>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
