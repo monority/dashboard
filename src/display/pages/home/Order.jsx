b7128de4df962e83c9eed25c99832dc6576ec905
monority <chenu.pro@gmail.com>
2025-05-06 10:44:30 +0200

    :sparkles: add Order page with revenue and subscription metrics; create corresponding SCSS file

--- src/display/pages/home/Order.jsx
+++ src/display/pages/home/Order.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import SmallBox from '../../components/ui/element/SmallBox'
 
 const Order = () => {
 	return (
@@ -6,7 +7,37 @@ const Order = () => {
 			<section className="task">
 				<div className="container">
 					<div className="wrapper_column gap1">
+						<h1>Orders</h1>
 					</div>
+					<div className="container_side">
+						<div className="wrapper_line">
+							<SmallBox
+								title="Total Revenue"
+								typeIcon="money"
+								desc="$45,000.00"
+								value="20,3"
+							/>
+							<SmallBox
+								title="Subscriptions"
+								typeIcon="money"
+								desc="2350"
+								value="11.7"
+							/>
+							<SmallBox
+								title="Sales"
+								typeIcon="money"
+								desc="12500"
+								value="5.7"
+							/>
+							<SmallBox
+								title="Active now"
+								typeIcon="money"
+								desc="125"
+								value="45"
+							/>
+						</div>
+					</div>
+
 				</div>
 			</section>
 		</>
