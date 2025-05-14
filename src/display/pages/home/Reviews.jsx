a43028578b23ea0cf6fcdc2708bb22ad1507e2fc
monority <chenu.pro@gmail.com>
2025-05-14 15:27:48 +0200

    :sparkles: update Reviews component; enhance layout with structured sections and improved styling

--- src/display/pages/home/Reviews.jsx
+++ src/display/pages/home/Reviews.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 
 const Reviews = () => {
-  return (
-	<>Reviews</>
-  )
+	return (
+		<>
+			<section className="reviews">
+				<div className="container">
+					<div className="wrapper">
+						<div className="element_between">
+							<div className="element">
+								<h1>Reviews</h1>
+							</div>
+							<div className="element">
+								<button className="btn btn-primary">March 2024 - February 2025</button>
+							</div>
+						</div>
+
+					</div>
+					<div className="container">
+						<div className="wrapper_between">
+							<div className="element_column">
+								<div className="element">
+
+								</div>
+								<div className="element">
+
+								</div>
+								<div className="element">
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	)
 }
 
 export default Reviews
