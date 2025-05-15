f83dc7fe6507b3faa5bbf95a4e38de50de673774
monority <chenu.pro@gmail.com>
2025-05-15 16:04:28 +0200

    :sparkles: - enhance Support component; add structured layout with headings and instructions for creating a new ticket

--- src/display/pages/home/Support.jsx
+++ src/display/pages/home/Support.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
 const Support = () => {
-  return (
-	<>Support</>
-  )
+	return (
+		<>
+			<section className="Support">
+				<div className="container">
+					<div className="wrapper">
+						<div className="element">
+							<h2>Support </h2>
+						</div>
+						<div className="element">
+							<p className="text_color02">Do you need help ?</p>
+						</div>
+					</div>
+					<div className="wrapper">
+						<div className="box_graph">
+							<div className="wrapper">
+								<div className="element">
+									<h4>Create new ticket</h4>
+								</div>
+								<div className="element">
+									<p className='text_color02'>Fill up the informations below</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+			</section>
+		</>
+	)
 }
 
 export default Support
