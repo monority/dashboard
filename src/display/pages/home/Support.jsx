8c024fc6ceb83b144de4a5a7f5c1adf8810bc6c7
monority <chenu.pro@gmail.com>
2025-05-19 20:09:40 +0200

    :art: - enhance Support component; refactor layout and add new styles for improved responsiveness and clarity

--- src/display/pages/home/Support.jsx
+++ src/display/pages/home/Support.jsx
@@ -12,8 +12,8 @@ const Support = () => {
 	];
 	return (
 		<>
-			<section className="Support">
-				<div className="container">
+			<section className="support">
+				<div className="container flex column gap2">
 					<div className="wrapper">
 						<div className="element">
 							<h2>Support </h2>
@@ -22,76 +22,75 @@ const Support = () => {
 							<p className="text_color02">Do you need help ?</p>
 						</div>
 					</div>
-					<div className="wrapper">
-						<div className="box_graph">
-							<div className="wrapper">
+
+					<div className="box_graph">
+						<div className="wrapper">
+							<div className="element">
+								<h4>Create new ticket</h4>
+							</div>
+							<div className="element">
+								<p className='text_color02'>Fill up the informations below</p>
+							</div>
+						</div>
+						<div className="wrapper">
+							<div className="container">
 								<div className="element">
-									<h4>Create new ticket</h4>
+									<h5>Select request type</h5>
 								</div>
 								<div className="element">
-									<p className='text_color02'>Fill up the informations below</p>
+									<SelectButton options={options} />
 								</div>
 							</div>
-							<div className="wrapper">
-								<div className="container">
-									<div className="element">
-										<h5>Select request type</h5>
-									</div>
-									<div className="element">
-										<SelectButton options={options} />
-									</div>
+							<div className="container">
+								<div className="element">
+									<h5>Subject</h5>
 								</div>
-								<div className="container">
-									<div className="element">
-										<h5>Subject</h5>
-									</div>
-									<div className="element">
-										<Input type="text" placeholder='Enter your problem' inputClassName="input_search" maxWidth="20rem" />
-									</div>
+								<div className="element">
+									<Input type="text" placeholder='Enter your problem' inputClassName="input_search" maxWidth="20rem" />
 								</div>
-								<div className="container">
-									<div className="element">
-										<h5>Date</h5>
-									</div>
-									<div className="element">
-										<Input type="date" placeholder='Enter your subject' inputClassName="input_search" maxWidth="20rem" />
-									</div>
+							</div>
+							<div className="container">
+								<div className="element">
+									<h5>Date</h5>
+								</div>
+								<div className="element">
+									<Input type="date" placeholder='Enter your subject' inputClassName="input_search" maxWidth="20rem" />
 								</div>
 							</div>
 						</div>
-						<div className="box_graph wrapper_between">
-							<div className="wrapper">
-								<div className="element"><h3>Number of issues</h3></div>
-								<div className="element"><p className='text_color02'>this month</p></div>
-								<div className="flex row gap1"><p>153</p>
-									<Badge colorBadge="success_light" label="10%" icon="true" /></div>
-							</div>
-							<div className="wrapper">
-								<div className="element"><h3>Issues solved</h3></div>
-								<div className="element"><p className='text_color02'>this month</p></div>
-
-								<div className="element">55</div>
-							</div>
-							<div className="wrapper">
-								<div className="element"><h3>Avg. response time</h3></div>
-								<div className="element"><p className='text_color02'>in last 7 days</p></div>
-
-								<div className="flex row gap1"><p>92%</p> <Badge colorBadge="negative" label="2%" iconRed="true" /></div>
-							</div>
-							<div className="wrapper">
-								<div className="element"><h3>Client satisfaction</h3></div>
-								<div className="element"><p className='text_color02'>in last 7 days</p></div>
-
-								<div className="flex row gap1">90% 	<Badge colorBadge="success_light" label="10%" icon="true" /></div>
-							</div>
-							<div className="wrapper">
-								<div className="element"><h3>Open tickets</h3></div>
-								<div className="element"><p className='text_color02'>in last project</p></div>
-
-								<div className="element">204</div>
-							</div>
-
+					</div>
+					<div className="box_graph wrapper_stats">
+						<div className="wrapper">
+							<div className="element"><h3>Number of issues</h3></div>
+							<div className="element"><p className='text_color02'>this month</p></div>
+							<div className="flex row gap1"><p>153</p>
+								<Badge colorBadge="success_light" label="10%" icon="true" /></div>
 						</div>
+						<div className="wrapper">
+							<div className="element"><h3>Issues solved</h3></div>
+							<div className="element"><p className='text_color02'>this month</p></div>
+
+							<div className="element">55</div>
+						</div>
+						<div className="wrapper">
+							<div className="element"><h3>Avg. response time</h3></div>
+							<div className="element"><p className='text_color02'>in last 7 days</p></div>
+
+							<div className="flex row gap1"><p>92%</p> <Badge colorBadge="negative" label="2%" iconRed="true" /></div>
+						</div>
+						<div className="wrapper">
+							<div className="element"><h3>Client satisfaction</h3></div>
+							<div className="element"><p className='text_color02'>in last 7 days</p></div>
+
+							<div className="flex row gap1">90% 	<Badge colorBadge="success_light" label="10%" icon="true" /></div>
+						</div>
+						<div className="wrapper">
+							<div className="element"><h3>Open tickets</h3></div>
+							<div className="element"><p className='text_color02'>in last project</p></div>
+
+							<div className="element">204</div>
+						</div>
+
 					</div>
 				</div>
 
