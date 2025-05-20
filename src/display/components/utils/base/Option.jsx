99b406c3c7cbe00ce35f8c3ad2b7dd4f7f55a578
monority <chenu.pro@gmail.com>
2025-05-20 15:13:09 +0200

    :zap: - refactor Option component; remove PropTypes and simplify rendering logic for improved readability and maintainability

--- src/display/components/utils/base/Option.jsx
+++ src/display/components/utils/base/Option.jsx
@@ -1,36 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const Option = React.memo(({
-	value,
-	disabled = false,
-	className = '',
-	children,
-	...rest
-}) => {
-	const optionClasses = ['option', className]
-		.filter(Boolean)
-		.join(' ');
-
-	return (
-		<option
-			value={value}
-			disabled={disabled}
-			className={optionClasses}
-			{...rest}
-		>
-			{children}
-		</option>
-	);
-});
-
-Option.propTypes = {
-	value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-	disabled: PropTypes.bool,
-	className: PropTypes.string,
-	children: PropTypes.node.isRequired,
-};
-
-Option.displayName = 'Option';
+    value,
+    disabled = false,
+    className = '',
+    children,
+    ...rest
+}) => (
+    <option
+        value={value}
+        disabled={disabled}
+        className={['option', className].filter(Boolean).join(' ')}
+        {...rest}
+    >
+        {children}
+    </option>
+));
 
 export default Option;
